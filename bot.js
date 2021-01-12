a247fbceaac006124944f88ee2fff7d532f2b273
jack2396 <43402038+jack2396@users.noreply.github.com>
2021-01-12 09:15:21 +0800

    Create bot.js

--- bot.js
+++ bot.js
@@ -19,10 +19,41 @@ app.listen(process.env.PORT || 5000);
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+function DataFetch(Str) {
+    return new Promise(function(resolve, reject) {
+        pool.query(Str, (err, res) => {
+            if (err) {
+                console.log(err.stack);
+                reject("Failed.");
+                throw err;
+            } else {
+                Fetch_result = res;
+                resolve(Fetch_result);
+            }
+        });
+    });
+}
+
 app.post('/login', urlencodedParser, function (req, res) {
-	console.log(req.body.account);
-	console.log(req.body.password);
-	res.send('hello world!');
+	console.log(req.body.content);
+	var username = req.body.content.split(", ")[0];
+	var pw = req.body.content.split(", ")[1];
+	var str = "SELECT " + id + " FROM account WHERE EXISTS ( SELECT * FROM account WHERE username = '" + username + "' ) LIMIT 1;";
+	DataFetch(str).then(res => {
+		if (typeof(res.rows[0]) == "undefined" || JSON.stringify(res.rows[0]).includes("null")) {
+			res.send('此帳號不存在。');
+    	} else {
+			var str = "SELECT " + id + " FROM account WHERE EXISTS ( SELECT * FROM account WHERE password = '" + pw + "' ) LIMIT 1;";
+			DataFetch(str).then(res => {
+				if (typeof(res.rows[0]) == "undefined" || JSON.stringify(res.rows[0]).includes("null")) {
+					res.send('密碼錯誤，請重新輸入。');
+  				} else {
+  					res.send(username + '，歡迎回來！');
+    		   	}
+			});
+        }
+	});
+	
 });
 
 app.post('/register', urlencodedParser, function (req, res) {
