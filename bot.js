0e40d93d10a9e5c3e7188318d897a766c052fb81
jack2396 <43402038+jack2396@users.noreply.github.com>
2021-01-12 11:21:33 +0800

    Create bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ app.post('/login', urlencodedParser, function (req, response) {
 		if (typeof(res.rows[0]) == "undefined" || JSON.stringify(res.rows[0]).includes("null")) {
 			response.send('此帳號不存在。');
     	} else {
-			var str = "SELECT " + id + " FROM account WHERE EXISTS ( SELECT * FROM account WHERE password = '" + pw + "' ) LIMIT 1;";
+			var str = "SELECT id FROM account WHERE EXISTS ( SELECT * FROM account WHERE password = '" + pw + "' ) LIMIT 1;";
 			DataFetch(str).then(res => {
 				if (typeof(res.rows[0]) == "undefined" || JSON.stringify(res.rows[0]).includes("null")) {
 					response.send('密碼錯誤，請重新輸入。');
@@ -53,11 +53,33 @@ app.post('/login', urlencodedParser, function (req, response) {
 			});
         }
 	});
-	
 });
 
 app.post('/register', urlencodedParser, function (req, res) {
-	res.send('');
+		console.log(req.body.content);
+	var username = req.body.content.split(", ")[0];
+	var pw = req.body.content.split(", ")[1];
+	var str = "SELECT username FROM account WHERE EXISTS ( SELECT * FROM account WHERE username = '" + username + "' ) LIMIT 1;";
+	DataFetch(str).then(res => {
+		if (typeof(res.rows[0]) == "undefined" || JSON.stringify(res.rows[0]).includes("null")) {
+			var str = "INSERT INTO account (username, password) VALUES ('" + username + "', '" + pw + "');";
+			pool.query(Str, (err, res) => {
+            	if (err) {
+               		console.log(err.stack);
+            	    reject("Failed.");
+                	throw err;
+           	 	} else {
+           	 		var id = "";
+           	 		for (var i = 0; i <= 16; i++) {
+           	 			id += Math.floor(Math.random() * 10) + 1;
+           	 		}
+					response.send('註冊成功！');
+        	    }
+        	});
+    	} else {
+			response.send('本帳號已存在！請試著使用其他帳號。');
+        }
+	});
 });
 
 app.post('/project', urlencodedParser, function (req, res) {
